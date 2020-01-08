e1a8b4f6e3615c48eb83ed0531991faa24e5a54e
zhanghudong <zh137663996@163.com>
2020-01-08 19:36:25 +0800

    util 上传

--- sms/aliyun/sms_code_test.go
+++ sms/aliyun/sms_code_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestSendSMSCode(t *testing.T) {
-	response, err := SendSMSCode("18842671207", "1266", SLLT)
+	response, err := SendSMSCode("18842671208", "1266", SLLT)
 	if err != nil {
 		log.Println("err", err)
 	}
